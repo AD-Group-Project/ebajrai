e0a7d6607f0e5e57f41837bffbe8c45740197016
adamatnoh <76959023+adamatnoh@users.noreply.github.com>
2021-11-22 02:13:23 +0800

    register login laravel

--- laravel/ebajrai/resources/views/auth/login.blade.php
+++ laravel/ebajrai/resources/views/auth/login.blade.php
@@ -1,4 +1,5 @@
-<x-guest-layout>
+{{--
+<!-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -45,4 +46,94 @@
             </div>
         </form>
     </x-jet-authentication-card>
+</x-guest-layout> -->
+--}}
+<x-guest-layout>
+
+    <!DOCTYPE html>
+    <html>
+        
+        <head>
+            
+            <title> E-Bajrai | Login </title>
+            <link rel="shortcut icon" href="images/logo.png">
+            <link rel="stylesheet" href="css/base_style.css">
+            <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+            <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+            <script src="https://kit.fontawesome.com/a7b35074e7.js" crossorigin="anonymous"></script>
+            <link rel="stylesheet" type="text/css" href="css/astyle.css">
+            <br><br>
+            <style>
+                
+                body {
+                    background-color: #F5F8F2;
+                    color: darkslategray;
+                    font-size: 15px;
+                }
+
+                .Signup .btn {
+                    width: 100%;
+                    margin: 10px 0px;
+                    font-size: 18px;
+                    background-color: #53B175;
+                    border: none;
+                }
+                
+                .user_card{
+                    height: 25%;
+                    width: 500px;
+                    padding: 80px;
+                }
+
+                footer
+                {
+                    position: absolute;
+                }
+
+            </style>
+            
+        </head>
+
+        <body>
+
+            <div class="container h-100">
+                <div class="d-flex justify-content-center h-100">
+                    <div class="user_card">
+                        <form action="{{route('login')}}" method="POST" name="frm-login" class="Signup">
+                            @csrf
+                            <div class="container">
+                                <div class="row"> 
+                                    <div class="col-sm">
+
+                                        <h2 style="text-align: center"> Login </h2>
+                                        <p style="text-align: center; color: gray"> Enter your credentials to continue </p>
+                                        <hr class="mb-3"><br>
+
+                                        <label for="email"> <b>Email Address</b> </label>
+                                        <input class="form-control" type="email" name="email" id="frm-login-uname" placeholder="Email address" :value="old('email')" required autofocus>
+
+                                        <label for="password"> <b>Password</b> </label>
+                                        <input class="form-control" type="password" name="password" placeholder="*******" required autocomplete="current-password">
+                                        <br>
+                                        <x-jet-validation-errors class="mb-4" />
+                                        <hr class="mb-3">
+                                        <input class="btn btn-primary" type="submit" id="register" name="create" value="Sign Up">
+                                        
+                                        <div class="mt-4">
+                                            <div class="d-flex justify-content-center links">
+                                                Don't have an account? &nbsp; <a href="{{route('register')}}" style="color: #53B175; text-decoration: none">Sign Up</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </body>
+        
+    </html>
+
 </x-guest-layout>
--- laravel/ebajrai/resources/views/layouts/base.blade.php
+++ laravel/ebajrai/resources/views/layouts/base.blade.php
@@ -4,6 +4,7 @@
     
     <head>
         <title> E-Bajrai Mini Market - All Item </title>
+        <link rel="shortcut icon" href="images/logo.png">
         <link rel="stylesheet" href="{{ asset('css/base_style.css') }}">
         <link rel="stylesheet" href="{{ asset('css/shop_style.css') }}">
         @livewireStyles
@@ -66,8 +67,9 @@
         
         {{$slot}}
         
-        <br><br>
-    
+        <footer>
+            <p>© Copyright 2021 Bajrai Mini Market, Inc.</p>
+        </footer>
     </body>
 
 </html>

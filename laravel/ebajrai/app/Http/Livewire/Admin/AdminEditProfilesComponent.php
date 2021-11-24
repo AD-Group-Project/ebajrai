8803fc805797f136f66f35e53482d954380a368e
farhahahmad <farhahbajrai@gmail.com>
2021-11-25 01:02:26 +0800

    database products

--- laravel/ebajrai/app/Http/Livewire/Admin/AdminEditProfilesComponent.php
+++ laravel/ebajrai/app/Http/Livewire/Admin/AdminEditProfilesComponent.php
@@ -4,7 +4,7 @@ namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
 
-class AdminEditShopInfo extends Component
+class AdminEditProfilesComponent extends Component
 {
     public $decs;
     public $monThu;
--- laravel/ebajrai/app/Http/Livewire/HomeComponent.php
+++ laravel/ebajrai/app/Http/Livewire/HomeComponent.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\Category;
 
 class HomeComponent extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.home-component')->layout('layouts.base');
+        $products = Product::paginate(16);
+        $categories = Category::all();
+        return view('livewire.home-component', ['products'=>$products, 'categories'=>$categories])->layout('layouts.base');
     }
 }

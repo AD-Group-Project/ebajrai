b7a57d08f35e24f15ac41b3f8fa1b3798c94f1d0
hasifhafifi <hasifhafifi20@gmail.com>
2021-12-01 17:28:22 +0800

    admin view product placement

--- laravel/ebajrai/app/Http/Livewire/DetailsComponent.php
+++ laravel/ebajrai/app/Http/Livewire/DetailsComponent.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use App\Models\Product;
 use Livewire\Component;
 use App\Models\Category;
+use Illuminate\Support\Facades\Auth;;
 
 class DetailsComponent extends Component
 {
@@ -19,6 +20,10 @@ class DetailsComponent extends Component
     {
         $product = Product::where('slug',$this->slug)->first();
         $categories = Category::all();
+        if(Auth::user()->utype === 'ADM')
+        {
+            return view('livewire.details-component',['product'=>$product], ['categories'=>$categories])->layout('livewire\admin\admin-dashboard-component');
+        }
         return view('livewire.details-component',['product'=>$product], ['categories'=>$categories])->layout('layouts.base');
     }
 }

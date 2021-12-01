2f99aee014913af3ec1bf63bdd92e458d807423e
hasifhafifi <hasifhafifi20@gmail.com>
2021-12-01 23:31:04 +0800

    update admin view details

--- laravel/ebajrai/app/Http/Livewire/Admin/AdminProductsComponent.php
+++ laravel/ebajrai/app/Http/Livewire/Admin/AdminProductsComponent.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\Category;
 
 class AdminProductsComponent extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.admin.admin-products-component')->layout('livewire\admin\admin-dashboard-component');
+        $products = Product::paginate(16);
+        $categories = Category::all();
+        return view('livewire.admin.admin-products-component', ['products'=>$products, 'categories'=>$categories])->layout('livewire\admin\admin-dashboard-component');
     }
 }
